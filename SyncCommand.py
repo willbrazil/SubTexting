c6bea32148ef3b86b2b3a3bdd0b1ce8a966c649a
Will Guedes <willbrazil.usa@gmail.com>
2015-03-30 20:54:25 -0400

    pinging stub endpoint to query for new messages.

--- SyncCommand.py
+++ SyncCommand.py
@@ -1,33 +1,48 @@
-import sublime, sublime_plugin, threading, sched, time
+import sublime, sublime_plugin, threading, sched, time, urllib.request as urllib2, json
 
-class SyncCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		thread = MessageSync(5, self.view, edit)
-		thread.start()
+class SyncCommand(sublime_plugin.ApplicationCommand):
+
+	def __init__(self):
+		self.running = False
+
+	def run(self):
+		if self.running:
+			print("Already running.")
+		else:
+			print('Started.')
+			thread = MessageSync(5)
+			thread.start()
+			self.running = True
 		#self.view.insert(edit, 0, "Hello, World!")
 
 class MessageSync(threading.Thread):
-	def __init__(self, timeout, view, edit):
+
+	def __init__(self, timeout):
 		self.timeout = timeout
 		self.result = None
-		self.view = view
-		self.edit = edit
 		threading.Thread.__init__(self)
-		self.message_animation = NewMessageAnimation() 
+		self.message_animation = NewMessageAnimation()
+		self.s = sched.scheduler(time.time, time.sleep)
 
-	def run(self):
-		self.message_animation.start()
-		print(self.view.file_name())
+	def run(self):	
+		self.ping()
 		#self.view.insert(self.edit, 0, "Inside thread!")
 		#self.result = 'Hey!!!!!!!'
 
-class NewMessageAnimation(threading.Thread):
-	def __init__(self):
-		threading.Thread.__init__(self)
+	def ping(self):
+		if self.has_new_message():
+	 		NewMessageAnimation().display_eye_catcher(1)
 
-	def run(self):
-		self.display_eye_catcher(1)
-		#self.display_anim_frame(1)
+		self.s.enter(2, 1, self.ping, ())
+		#self.s.run()
+
+	def has_new_message(self):
+		res = urllib2.urlopen('http://localhost:5000')
+		if res.code == 200:
+			return json.loads(res.read().decode('utf-8'))['message_count'] > 0
+		return False
+
+class NewMessageAnimation():
 
 	def display_eye_catcher(self, frame):
 		spaces = frame % 200
