c779b7f75235097b678af65bc53f622eef0c6595
Guilherme Guedes <willbrazil.usa@gmail.com>
2015-04-03 19:55:25 -0400

    Added information to verify registration.

--- RegisterCommand.py
+++ RegisterCommand.py
@@ -5,40 +5,37 @@ import imp
 class RegisterCommand(sublime_plugin.WindowCommand):
 	def run(self):
 		imp.reload(util)
-		print(util.get_pref('password'))
+		self.phone = None
 		self.username = None
-		self.password = None
 		self.get_username()
 
-	def get_username(self, caption='Username: '):
-		self.window.show_input_panel(caption, '', self.handle_username, None, None)
+	def get_username(self, error_msg=''):
+		self.window.show_input_panel("%s Username: " % error_msg, '', self.handle_username, None, None)
 
 	def handle_username(self, content):
 		if content == '':
-			self.get_username('(Cannot be empty) Username: ')
+			self.get_username('(Cannot be empty)')
 		else:
 			self.username = content
-			self.get_password()
+			self.get_phone()
 
-	def get_password(self, caption='Password: '):
-		self.window.show_input_panel(caption, '', self.handle_password, None, None)
+	def get_phone(self, error_msg=''):
+		self.window.show_input_panel("%s Phone (we will NOT store your number!): " % error_msg, '5745142948', self.handle_phone, None, None)
 
-	def handle_password(self, content):
-		if content == '':
-			self.get_password('(Cannot be empty) Password: ')
+	def handle_phone(self, content):
+		if len(content) != 10:
+			self.get_phone('(Invalid number)')
 		else:
-			self.password = content
+			self.phone = content
 			self.register()
 
 	def register(self):
-		sublime.status_message("%s:%s" % (self.username, self.password))
-
-		data = {'username': self.username}
+		data = {'username': self.username, 'phone': self.phone}
 		res = urllib.request.urlopen('http://localhost:5000/signup', data=urllib.parse.urlencode(data).encode('utf-8'))
 		res_data = res.read().decode('utf-8')
 		if res_data == 'OK':
 			util.set_pref('username', self.username)
-			util.set_pref('password', self.password)
-			sublime.message_dialog('Successfully Registered!')
+			util.set_pref('phone', self.phone)
+			sublime.message_dialog('A verification text has been sent to your phone. Please run "CTRL+SHIFT+P -> Verify" once you have the code.')
 		else:
 			sublime.message_dialog(res_data)
